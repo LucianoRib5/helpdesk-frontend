a0f5d64da9416c1295ac50ec4b4c66802b051f4f
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-26 21:58:18 -0300

    feat: integrate Material UI theme provider in main.tsx and create theme configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import App from './App.tsx'
+import theme from './styles/theme.ts';
 
 const queryClient = new QueryClient()
 
@@ -11,7 +13,10 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
       </QueryClientProvider>
     </Provider>
   </StrictMode>,
