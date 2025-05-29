e53cee746c7f26da28d674ea8a2ec490482b1d63
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-29 15:46:10 -0300

    feat: add CustomBox component for reusable layout

---
+++ src/components/CustomBox.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box, type BoxProps } from '@mui/material';
+
+interface CustomBoxProps extends BoxProps {
+  children: React.ReactNode;
+}
+
+const CustomBox: React.FC<CustomBoxProps> = ({ children, ...rest }) => {
+  return (
+    <Box {...rest}>
+      {children}
+    </Box>
+  );
+};
+
+export default CustomBox;
