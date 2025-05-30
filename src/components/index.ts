309c0ca9b8bcb3517a39a9cb936350269d27396a
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-29 23:41:48 -0300

    feat: create index file to export component modules

---
+++ src/components/index.ts
@@ -0,0 +1,25 @@
+import CheckboxField from "./CheckboxField";
+import CustomAppBar from "./CustomAppBar";
+import CustomBox from "./CustomBox";
+import CustomButton from "./CustomButton";
+import CustomDrawer from "./CustomDrawer";
+import CustomInput from "./CustomInput";
+import CustomList from "./CustomList";
+import CustomPaper from "./CustomPaper";
+import CustomText from "./CustomText";
+import CustomToolbar from "./CustomToolbar";
+import NavListItem from "./NavListItem";
+
+export {
+    CheckboxField,
+    CustomAppBar,
+    CustomBox,
+    CustomButton,
+    CustomDrawer,
+    CustomInput,
+    CustomList,
+    CustomPaper,
+    CustomText,
+    CustomToolbar,
+    NavListItem, 
+};
