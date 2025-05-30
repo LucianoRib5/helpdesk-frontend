fdcd624eb21ca8bacae3afdba3ca1908ca0dd0b3
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-30 00:30:43 -0300

    feat: add icon prop to NavListItem for enhanced functionality

--- src/components/CustomToolbar.tsx
+++ src/components/CustomToolbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Toolbar, type ToolbarProps } from '@mui/material';
 
 interface CustomToolbarProps extends ToolbarProps {
--- src/components/NavListItem.tsx
+++ src/components/NavListItem.tsx
@@ -1,18 +1,31 @@
-import { ListItem, ListItemText, type ListItemProps } from '@mui/material';
+import { ListItem, ListItemIcon, ListItemText, type ListItemProps } from '@mui/material';
 
 interface NavListItemProps extends ListItemProps {
   text: string;
+  icon?: React.ReactNode;
   onClick?: () => void;
 }
 
-const NavListItem: React.FC<NavListItemProps> = ({ text, onClick, ...rest }) => {
+const NavListItem: React.FC<NavListItemProps> = ({ text, icon, onClick, ...rest }) => {
   return (
     <ListItem
       button
       component="button"
       onClick={onClick}
+      sx={{
+        border: 'none',
+        backgroundColor: 'transparent',
+        '&:hover .MuiListItemText-root': {
+          color: 'orange',
+        },
+      }}
       {...rest}
     >
+      {icon && (
+        <ListItemIcon sx={{ minWidth: '32px', color: 'inherit' }}>
+          {icon}
+        </ListItemIcon>
+      )}
       <ListItemText primary={text} />
     </ListItem>
   );
