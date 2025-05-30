e0be61d4030e68edab423d00c93234f499481257
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-29 23:43:04 -0300

    feat: replace Box with CustomBox in AuthLayout for consistent styling

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,11 +1,11 @@
-import { Box } from "@mui/material"
 import { Outlet } from "react-router-dom"
+import { CustomBox } from "../components"
 
 const AuthLayout = () => {
   return (
-    <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh" bgcolor="#f3f3f3">
+    <CustomBox display="flex" justifyContent="center" alignItems="center" minHeight="100vh" bgcolor="#f3f3f3">
       <Outlet />
-    </Box>
+    </CustomBox>
   )
 }
 
