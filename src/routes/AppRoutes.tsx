22cd606e869b4d8f28c7f7739d1c3f955d1b18ea
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-31 15:40:24 -0300

    feat: add authentication check for root route navigation

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import UserRegister from '../pages/UserRegister/UserRegister'
 import ProtectedRoute from '../components/ProtectedRoute'
 import MainLayout from '../layouts/MainLayout'
@@ -7,9 +7,16 @@ import NotFound from '../pages/NotFound/NotFound'
 import Login from '../pages/Login/Login'
 import AuthLayout from '../layouts/AuthLayout'
 
+const isAuthenticated = () => Boolean(localStorage.getItem('token'))
+
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route path='/' element={
+        isAuthenticated()
+          ? <Navigate to='/dashboard' replace />
+          : <Navigate to='/login' replace />
+      } />
       <Route element={<AuthLayout />}>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<UserRegister />} />
