d45c7b922e815ceaea232a2f00514ac7c4d88149
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-05-31 13:58:10 -0300

    feat: implement customer data retrieval and display NewTicketForm in Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,38 @@
+import { useQuery } from "@tanstack/react-query";
+import { CustomBox } from "../../components";
+import { NewTicketForm } from "../../components/NewTicketForm";
+import { UserTypeEnum } from "../../features/user/userTypes";
+import { useAppSelector } from "../../hooks/useAppSelector";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { useEffect } from "react";
+import { setCustomers } from "../../store/slices/customerSlice";
+import CustomerService from "../../services/CustomerService";
+
 const Home: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { user } = useAppSelector((state) => state.auth);
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['customers'],
+        queryFn: () => CustomerService.getAllCustomers(),
+        enabled: user?.userType !== UserTypeEnum.CUSTOMER,
+    });
+
+    useEffect(() => {
+        if (data && !isLoading && !isError) {
+            dispatch(setCustomers(data));
+        }
+    }, [dispatch]);
+
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <CustomBox sx={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            {user && (
+                <NewTicketForm user={user}/>
+            )}
+        </CustomBox>
     );
 }
 
