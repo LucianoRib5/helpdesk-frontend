62a5fa0ba8d4c404ac675b9cc926991d2895c014
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-06-01 13:31:41 -0300

    feat: add LastTickets component to display the latest tickets

---
+++ src/components/LastTickets.tsx
@@ -0,0 +1,56 @@
+import { List, ListItem, ListItemText } from '@mui/material';
+import { CustomBox, CustomPaper, CustomText } from '../components';
+import { useAppSelector } from '../hooks/useAppSelector';
+
+const LastTickets: React.FC = () => {
+  const { tickets } = useAppSelector((state) => state.ticket);
+
+  const orderedLastTickets = [...tickets]
+    .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+    .slice(0, 3);
+
+  return (
+    <CustomPaper
+      elevation={1}
+      sx={{
+        p: 4,
+        borderRadius: 3,
+        maxWidth: 800,
+        boxShadow: '0px 2px 10px rgba(0,0,0,0.05)',
+      }}
+    >
+      <CustomText variant='h6' fontWeight='bold' gutterBottom>
+        Últimos chamados abertos
+      </CustomText>
+      <CustomText variant='body2' color='text.secondary' gutterBottom>
+        Confira seus três últimos chamados abertos, você pode acessa-los a qualquer momento!
+      </CustomText>
+      <CustomBox>
+        <List disablePadding>
+          {
+            orderedLastTickets.length > 0 ? (
+              orderedLastTickets.map((ticket) => (
+                <ListItem key={ticket.id} disableGutters disablePadding>
+                  <ListItemText
+                  primary={
+                    <CustomText variant='body2' fontWeight={500}>
+                    {`#${String(ticket.id).padStart(3, '0')} - ${ticket.title}`}
+                    </CustomText>
+                  }
+                  />
+                </ListItem>
+              ))
+            ) :
+            (
+              <CustomText variant='body2'>
+                Você ainda não possui chamados abertos.
+              </CustomText>
+            )
+          }
+        </List>
+      </CustomBox>
+    </CustomPaper>
+  );
+};
+
+export default LastTickets;
