d760c5e77e87397b3710b59420af140782aa7491
Luciano Ribeiro <luciano.eng.soft@outlook.com>
2025-06-01 09:26:59 -0300

    feat: add ticket retrieval methods to TicketService

--- src/services/TicketService.ts
+++ src/services/TicketService.ts
@@ -1,12 +1,18 @@
 import type { CreateTicketPayload, Ticket } from '../features/ticket/ticketTypes'
 import ApiService from './ApiService'
 
-const { post } = ApiService
+const { post, get } = ApiService
 
 const TicketService = {
     createTicket: async (payload: CreateTicketPayload) => {
         return post<Ticket>('/tickets', payload)
     },
+    getTicketsByCustomerId: async (customerId: number) => {
+        return get<Ticket[]>(`/tickets/customer/${customerId}`, {})
+    },
+    getAllTickets: async () => {
+        return get<Ticket[]>('/tickets', {})
+    }
 }
 
 export default TicketService
